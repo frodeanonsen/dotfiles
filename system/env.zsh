c33d5b73745205738c5c3f0ba83edfdd18ba2467
Frode Ånonsen <frode@frodembp.local>
2018-06-12 23:52:53 +0200

    Updates from jldeen mac dotfiles

--- system/env.zsh
+++ system/env.zsh
@@ -1,5 +1,1 @@
-# Only set this if we haven't set $EDITOR up somewhere else previously.
-if [[ "$EDITOR" == "" ]] ; then
-  # Use vscode for my editor.
-  export EDITOR='code'
-fi
+export EDITOR='vim'
