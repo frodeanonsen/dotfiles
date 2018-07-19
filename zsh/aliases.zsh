55904a2b7c01ce52c16f6e7b12a426e7169820bc
Frode Anonsen <frode@anonsen.org>
2018-07-19 10:32:35 +0200

    Better ls -l, ls -la aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,7 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+# NB! Using `gls -F --color` from oh-my-zsh config
+alias ll='ls -lh --group-directories-first'
+alias la='ls -lah --group-directories-first'
