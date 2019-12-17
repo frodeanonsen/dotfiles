49da38fdb557f8ec30b964524bc64b406bd6dfbd
Frode Anonsen <frode@anonsen.org>
2019-12-17 21:39:48 +0000

    tweak zsh

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -61,7 +61,7 @@ battery_status() {
   fi
 }
 
-export PROMPT=$'\n$(battery_status)in $(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT=$'\n$(directory_name) $(git_dirty)$(need_push)\n› '
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
 }
