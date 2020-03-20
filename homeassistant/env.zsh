94d01817a7266fbbb23787f335aa184d020b1e55
Frode Anonsen <frode@anonsen.org>
2020-03-20 12:04:08 +0000

    check for hass-cli

--- homeassistant/env.zsh
+++ homeassistant/env.zsh
@@ -1,4 +1,4 @@
-if command -v yq >/dev/null 2>&1; then
+if command -v yq >/dev/null 2>&1 && command -v hass-cli >/dev/null 2>&1; then
   export HASS_SERVER=`yq r ~/.dotfiles/secret.yaml hass.url`
   export HASS_TOKEN=`yq r ~/.dotfiles/secret.yaml hass.token`
 fi
