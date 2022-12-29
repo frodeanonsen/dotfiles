79f5a6db8b0eba2f0f7e808a44d93fbfb5e7ff5f
Frode Anonsen <frode@anonsen.org>
2022-12-29 08:45:33 +0100

    make sure pyenv is added to path

--- python/pyenv.zsh
+++ python/pyenv.zsh
@@ -1,3 +1,4 @@
 export PYENV_ROOT="$HOME/.pyenv"
-command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+# command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
