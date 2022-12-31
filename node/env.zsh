f583f473450e0ef88fddb350c621c2f324151ac8
Frode Anonsen <frode@anonsen.org>
2022-12-31 10:37:47 +0100

    disable nvm for now

---
+++ node/env.zsh
@@ -0,0 +1,2 @@
+alias serve="browser-sync start -s -f . --no-notify --host `ipconfig getifaddr en0` --port 9000"
+PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
--- node/nvm.zsh
+++ node/nvm.zsh
@@ -1,6 +1,9 @@
-if [[ -d ~/.nvm ]]; then
-  export NVM_DIR="$HOME/.nvm"
-  if [[ -f "$NVM_DIR/nvm.sh" ]]; then
-    . "$NVM_DIR/nvm.sh"
-  fi
-fi
+# Remove because slow start
+#if [[ -d ~/.nvm ]]; then
+#   export NVM_DIR="$HOME/.nvm"
+#   if [[ -f "$NVM_DIR/nvm.sh" ]]; then
+#     . "$NVM_DIR/nvm.sh"
+#     . "$NVM_DIR/bash_completion"
+#     nvm use default
+#   fi
+# fi
