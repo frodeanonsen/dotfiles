c3e7af693d11ba3f00a73d6424704b04a2cce300
Frode Anonsen <frode@anonsen.org>
2023-01-04 12:22:44 +0100

    node

--- node/env.zsh
+++ node/env.zsh
@@ -1,2 +1,7 @@
-alias serve="browser-sync start -s -f . --no-notify --host `ipconfig getifaddr en0` --port 9000"
-PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
+if [ $commands[ipconfig] ]; then
+  alias serve="browser-sync start -s -f . --no-notify --host `ipconfig getifaddr en0` --port 9000"
+fi
+
+if [ $commands[yarn] ]; then
+  PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
+fi
