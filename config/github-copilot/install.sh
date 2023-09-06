25d8aed4b4f1659f79a3a60ee41d801fc7938ba3
Frode Anonsen <frode@anonsen.org>
2023-09-06 19:06:20 +0200

    gh cli and copilot

---
+++ config/github-copilot/install.sh
@@ -0,0 +1,17 @@
+edir=~/.config/github-copilot
+efile=$edir/hosts.json
+
+if [[ ! -d "$edir" ]]; then
+  mkdir "$edir"
+fi
+
+if [[ ! -f "$efile" ]]; then
+  cat <<EOF > $efile
+{
+  "github.com": {
+    "user": "$(op read 'op://private/github.com/gh-username')",
+    "oauth_token": "$(op read 'op://private/github.com/copilot-token')"
+  }
+}
+EOF
+fi
