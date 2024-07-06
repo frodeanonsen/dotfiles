11b94295dbe2f87a7c16925acd48ecfef6aa41b9
Frode Ånonsen <frode@anonsen.org>
2024-07-06 14:11:59 +0200

    podman check for vm

--- podman/env.zsh
+++ podman/env.zsh
@@ -1,6 +1,8 @@
 DOCKER_HOST=unix://
 if [[ $(uname) == "Darwin" ]]; then
-  DOCKER_HOST="${DOCKER_HOST}`podman machine inspect --format '{{.ConnectionInfo.PodmanSocket.Path}}'`"
+  if [[ `podman system connection list | grep ssh` == 0 ]]; then
+    DOCKER_HOST="${DOCKER_HOST}`podman machine inspect --format '{{.ConnectionInfo.PodmanSocket.Path}}'`"
+  fi
 else
   DOCKER_HOST="${DOCKER_HOST}`podman info --format '{{.Host.RemoteSocket.Path}}'`"
 fi
