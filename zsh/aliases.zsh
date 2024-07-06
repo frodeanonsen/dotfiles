f92bd3ef0ff207802dad01f408025fe9230745b3
Frode Ånonsen <frode@anonsen.org>
2024-07-06 14:05:42 +0200

    exa to eza

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,9 +2,9 @@ alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
 
-alias ll='exa -l --icons --git --group-directories-first --time-style=long-iso'
-alias la='exa -l --icons --git -a --group-directories-first --time-style=long-iso'
-alias lt='exa --tree --level=2 --icons --git -a --group-directories-first --time-style=long-iso'
+alias ll='eza -l --icons --git --group-directories-first --time-style=long-iso'
+alias la='eza -l --icons --git -a --group-directories-first --time-style=long-iso'
+alias lt='eza --tree --level=2 --icons --git -a --group-directories-first --time-style=long-iso'
 
 alias du=dust
 
