8ae3d05e8d71e9faf0b56478b18af13433c80cc6
Frode Anonsen <frode@anonsen.org>
2024-08-27 09:20:35 +0200

    zsh cleanup

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -5,6 +5,7 @@ alias cls='clear' # Good 'ol Clear Screen command
 alias ll='eza -l --icons --git --group-directories-first --time-style=long-iso'
 alias la='eza -l --icons --git -a --group-directories-first --time-style=long-iso'
 alias lt='eza --tree --level=2 --icons --git -a --group-directories-first --time-style=long-iso'
+alias lastmod='find . -type f -not -path "*/\.*" -exec eza -lrt modified --icons --git --group-directories-first --time-style=long-iso {} + | head -n 10'
 
 alias du=dust
 
