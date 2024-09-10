665b8b57642385c82ef5154cda0418d7da8cbc0d
Frode Anonsen <frode@anonsen.org>
2024-09-10 07:59:07 +0200

    clean up paths and aliases

--- config/system/path.zsh
+++ config/system/path.zsh
@@ -1,3 +1,3 @@
-export PATH="./bin:$HOME/Library/Python/3.11/bin:$HOME/.rbenv/shims:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH="./bin:$HOME/.cargo/bin:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -6,7 +6,7 @@ alias cls='clear' # Good 'ol Clear Screen command
 alias ll='eza -l --icons --git --group-directories-first --time-style=long-iso'
 alias la='eza -l --icons --git -a --group-directories-first --time-style=long-iso'
 alias lt='eza --tree --level=2 --icons --git -a --group-directories-first --time-style=long-iso'
-alias lastmod='find . -type f -not -path "*/\.*" -exec eza -lrt modified --icons --git --group-directories-first --time-style=long-iso {} + | head -n 10'
+alias lastmod='eza -l -t modified -s newest -r --icons --git --time-style=long-iso --recurse |head -n 10'
 
 alias du=dust
 
