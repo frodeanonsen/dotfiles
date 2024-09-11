588214a5b6a57fc04ddabd7a793912596b8ae309
Frode Anonsen <frode@anonsen.org>
2024-09-11 21:46:26 +0200

    new token

--- config/zygizo/env.zsh
+++ config/zygizo/env.zsh
@@ -3,4 +3,5 @@ if command -v yq >/dev/null 2>&1; then
   export ARM_CLIENT_SECRET=`cat ~/.dotfiles/secret.yaml | yq '.zygizo.tfsp_password' -r`
   export ARM_SUBSCRIPTION_ID=`cat ~/.dotfiles/secret.yaml | yq '.zygizo.tfsp_subscription' -r`
   export ARM_TENANT_ID=`cat ~/.dotfiles/secret.yaml | yq '.zygizo.tfsp_tenant' -r`
+  export CR_PAT=`cat ~/.dotfiles/secret.yaml | yq '.zygizo.github_token' -r`
 fi
