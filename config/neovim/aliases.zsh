01daae9a8f432d836dd9a6b5f819df690ce2c3be
Frode Anonsen <frode@anonsen.org>
2024-12-02 09:10:48 +0100

    obsidian aliases

--- config/neovim/aliases.zsh
+++ config/neovim/aliases.zsh
@@ -2,6 +2,6 @@ alias vi=nvim
 alias vim=nvim
 
 # Obsidian vaults
-alias obzyg="nvim /Users/frode/Library/Mobile\ Documents/iCloud~md~obsidian/Documents/Zygizo"
-alias obzet="nvim /Users/frode/Library/Mobile\ Documents/iCloud~md~obsidian/Documents/Example"
+alias obzyg="cd /Users/frode/Library/Mobile\ Documents/iCloud~md~obsidian/Documents/Zygizo && nvim . && cd -"
+alias obzet="cd /Users/frode/Library/Mobile\ Documents/iCloud~md~obsidian/Documents/Example && nvim . && cd -"
 
