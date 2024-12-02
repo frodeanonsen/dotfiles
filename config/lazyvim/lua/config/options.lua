025f4c06de2c8f610600339d98c1aecb0754b789
Frode Anonsen <frode@anonsen.org>
2024-12-02 10:09:35 +0100

    manage clipboard over ssh

--- config/lazyvim/lua/config/options.lua
+++ config/lazyvim/lua/config/options.lua
@@ -3,3 +3,27 @@
 -- Add any additional options here
 
 vim.opt.colorcolumn = "120"
+
+function my_paste(reg)
+  return function(lines)
+    local content = vim.fn.getreg('"')
+    return vim.split(content, "\n")
+  end
+end
+
+if os.getenv("SSH_TTY") == nil then
+  vim.opt.clipboard:append("unnamedplus")
+else
+  vim.opt.clipboard:append("unnamedplus")
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = my_paste("+"),
+      ["*"] = my_paste("*"),
+    },
+  }
+end
