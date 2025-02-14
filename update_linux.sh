78840aaaae0b43345878b12aec678c389b27dacd
Frode Anonsen <frode.anonsen@zygizo.com>
2025-02-14 08:22:07 +0100

    update with retry

--- update_linux.sh
+++ update_linux.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env sh
-
 cd ~/.dotfiles || exit
-nix flake update
-home-manager switch --flake .#ZygizoOne
+
+if [ "$1" = "--flake" ]; then
+	nix flake update
+fi
+
+retries=0
+max_retries=10
+while ! home-manager switch --flake .#ZygizoOne; do
+	retries=$((retries + 1))
+	if [ "$retries" -ge "$max_retries" ]; then
+		echo "Reached maximum retries, exiting."
+		exit 1
+	fi
+	echo "running again"
+done
