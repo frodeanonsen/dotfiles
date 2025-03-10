12172f88d243fc775b9ec8df7350e156db00a327
Frode Anonsen <frode@anonsen.org>
2025-03-10 06:21:59 +0100

    rustowl

--- config/lazyvim/lua/plugins/rust.lua
+++ config/lazyvim/lua/plugins/rust.lua
@@ -4,7 +4,14 @@ return {
     dependencies = { "neovim/nvim-lspconfig" },
     config = function()
       local lspconfig = require("lspconfig")
-      lspconfig.rustowlsp.setup({})
+      lspconfig.rustowl.setup({})
+
+      vim.api.nvim_set_hl(0, "lifetime", { underline = true, sp = "#00ff00" })
+      vim.api.nvim_set_hl(0, "imm_borrow", { underline = true, sp = "#0000ff" })
+      vim.api.nvim_set_hl(0, "mut_borrow", { underline = true, sp = "#ff00ff" })
+      vim.api.nvim_set_hl(0, "move", { underdouble = true, sp = "#ffff00" })
+      vim.api.nvim_set_hl(0, "call", { underdouble = true, sp = "#ffff00" })
+      vim.api.nvim_set_hl(0, "outlive", { underline = true, sp = "#ff0000" })
     end,
   },
 }
