3cea2451eb3835260388f8c3ca01399cf1fadbec
Frode Anonsen <frode@anonsen.org>
2025-08-05 14:51:32 +0200

    lazyvim

--- config/lazyvim/lua/plugins/copilot.lua
+++ config/lazyvim/lua/plugins/copilot.lua
@@ -1,22 +1,43 @@
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  build = ":Copilot auth",
-  event = "InsertEnter",
-  opts = {
-    suggestion = {
-      enabled = not vim.g.ai_cmp,
-      auto_trigger = true,
-      keymap = {
-        accept = false, -- handled by nvim-cmp / blink.cmp
-        next = "<M-]>",
-        prev = "<M-[>",
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    event = "InsertEnter",
+    opts = {
+      suggestion = {
+        enabled = not vim.g.ai_cmp,
+        auto_trigger = true,
+        keymap = {
+          accept = false, -- handled by nvim-cmp / blink.cmp
+          next = "<M-]>",
+          prev = "<M-[>",
+        },
+      },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = false,
+        help = true,
       },
     },
-    panel = { enabled = false },
-    filetypes = {
-      markdown = false,
-      help = true,
+  },
+  {
+    "saghen/blink.cmp",
+    optional = true,
+    dependencies = { "giuxtaposition/blink-cmp-copilot" },
+    opts = {
+      sources = {
+        default = { "copilot" },
+        providers = {
+          copilot = {
+            name = "copilot",
+            module = "blink-cmp-copilot",
+            kind = "Copilot",
+            score_offset = 100,
+            async = true,
+          },
+        },
+      },
     },
   },
 }
