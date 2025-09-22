be38395211a48b0b193499db848e6a01cdf1050b
Frode Anonsen <frode@anonsen.org>
2025-09-22 10:01:48 +0200

    updates

--- config/lazyvim/lua/plugins/ros2.lua
+++ config/lazyvim/lua/plugins/ros2.lua
@@ -1,14 +1,20 @@
 return {
-  "ErickKramer/nvim-ros2",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-telescope/telescope.nvim",
-    "nvim-treesitter/nvim-treesitter",
-  },
-  opts = {
-    -- Add any custom options here
-    autocmds = true,
-    telescope = true,
-    treesitter = true,
-  },
+  -- DISABLED, does not work with current treesitter/main
+  -- Local fork
+  --
+  -- "frodeanonsen/nvim-ros2",
+  -- dir = "~/git/frode/nvim/nvim-ros2", -- Path to your local clone
+  -- dependencies = {
+  --   "nvim-lua/plenary.nvim",
+  --   -- "nvim-telescope/telescope.nvim",
+  --   "ibhagwan/fzf-lua",
+  --   "nvim-treesitter/nvim-treesitter",
+  -- },
+  -- opts = {
+  --   -- Add any custom options here
+  --   autocmds = true,
+  --   -- telescope = false,
+  --   picker = "fzf-lua",
+  --   treesitter = true,
+  -- },
 }
