be4c7f4f42ea59fcf06d90c1f2880ae23f94edf5
Frode Anonsen <frode@anonsen.org>
2025-09-22 10:02:15 +0200

    use sudo for darwin-rebuild switch

--- scripts/update_zygbook.sh
+++ scripts/update_zygbook.sh
@@ -7,7 +7,7 @@ fi
 
 retries=0
 max_retries=10
-while ! darwin-rebuild switch --flake .#MacBookApple; do
+while ! sudo darwin-rebuild switch --flake .#MacBookApple; do
 	retries=$((retries + 1))
 	if [ "$retries" -ge "$max_retries" ]; then
 		echo "Reached maximum retries, exiting."
